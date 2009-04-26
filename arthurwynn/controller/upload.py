a7417dc9a59f587ba332e25c57e9d641c4e8a63c
Phil Wills <otherphil@gmail.com>
2009-04-26 16:13:44 +0100

    Broke xml extractor out of uploader and added some tests for the extraction

--- arthurwynn/controller/upload.py
+++ arthurwynn/controller/upload.py
@@ -1,11 +1,8 @@
-import cgi
-import os
-
 from arthurwynn.controller.modelview import ModelAndViewPage
 
 from arthurwynn.crossword import *
 
-from xml.etree.ElementTree import *
+from xml.etree.ElementTree import fromstring
 
 class CrosswordUploadPage(ModelAndViewPage):
 	def get(self):
@@ -13,35 +10,67 @@ class CrosswordUploadPage(ModelAndViewPage):
 	
 	def post(self):
 		crossword = Crossword()
-		root = fromstring(self.request.get('xml'))
-		namespace = '{http://crossword.info/xml/rectangular-puzzle}'
-		puzzle = root.find('./' + namespace + 'rectangular-puzzle/')
-		title = puzzle.find('./' + namespace + 'metadata/' +namespace + 'title')
-		crossword.name = title.text
-		if title.text[0:4] == 'gdn.':
-			crossword.type = title.text[4:]
-		creator = puzzle.find('./' + namespace + 'metadata/' +namespace + 'creator')
-		crossword.creator = creator.text
-		identifier = puzzle.find('./' + namespace + 'metadata/' +namespace + 'identifier')
-		crossword.number = int(identifier.text)
-		grid = puzzle.find('./' + namespace + 'crossword/' +namespace + 'grid')
-		crossword.size(int(grid.get('width')))
+		extractor = CrosswordDotInfoXmlExtractor(self.request.get('xml'))
+		crossword.name = extractor.title()
+		crossword.type = extractor.type()
+		crossword.creator = extractor.creator()
+		crossword.number = extractor.identifier()
+		crossword.size(extractor.width())
+		crossword.build_solution(extractor.letters())
+		crossword.build_clues(extractor.across_clues(), extractor.down_clues())
+		crossword.put()
+		self.redirect('/crossword?key=' + str(crossword.key()))
+
+class CrosswordDotInfoXmlExtractor():
+	def __init__(self, xmlString):
+		self.root = fromstring(xmlString)
+		self.namespace = '{http://crossword.info/xml/rectangular-puzzle}'
+		self.puzzle = self.root.find('./' + self.namespace + 'rectangular-puzzle/')
+		self.grid = self.puzzle.find('./' + self.namespace + 'crossword/' + self.namespace + 'grid')
+		self.across_clue_dict = {}
+		self.down_clue_dict = {}
+		self.extract_clues()
+
+	def title(self):
+		return self.puzzle.find('./' + self.namespace + 'metadata/' + self.namespace + 'title').text
+
+	def type(self):
+		titleText = self.title()
+		if titleText[0:4] == 'gdn.':
+			return titleText[4:]
+
+	def creator(self):
+		return self.puzzle.find('./' + self.namespace + 'metadata/' + self.namespace + 'creator').text
+		
+	def identifier(self):
+		return int(self.puzzle.find('./' + self.namespace + 'metadata/' + self.namespace + 'identifier').text)
+		
+	def width(self):
+		return int(self.grid.get('width'))
+		
+	def height(self):
+		return int(self.grid.get('height'))
+
+	def letters(self):
 		letters = {}
-		for cell in grid.findall('./' + namespace + 'cell'):
+		for cell in self.grid.findall('./' + self.namespace + 'cell'):
 			if cell.get('solution'):
 				letters[int(cell.get('x')), int(cell.get('y'))] = cell.get('solution')
 			else:
 				letters[int(cell.get('x')), int(cell.get('y'))] = ''
-		crossword.build_solution(letters)
-		across_clues = {}
-		down_clues = {}
-		for clues in puzzle.findall('./' + namespace + 'crossword/' + namespace + 'clues'):
-			if (clues.find('./' + namespace + 'title/' + namespace +'b').text == 'Across'):
-				for clue in clues.findall('./' + namespace + 'clue'):
-					across_clues[clue.get('number')] = clue.text + ' (' + clue.get('format') + ')'
+		return letters
+
+	def across_clues(self):
+		return self.across_clue_dict
+
+	def down_clues(self):
+		return self.down_clue_dict
+
+	def extract_clues(self):
+		for clues in self.puzzle.findall('./' + self.namespace + 'crossword/' + self.namespace + 'clues'):
+			if (clues.find('./' + self.namespace + 'title/' + self.namespace +'b').text == 'Across'):
+				for clue in clues.findall('./' + self.namespace + 'clue'):
+					self.across_clue_dict[clue.get('number')] = clue.text + ' (' + clue.get('format') + ')'
 			else:
-				for clue in clues.findall('./' + namespace + 'clue'):
-					down_clues[clue.get('number')] = clue.text + ' (' + clue.get('format') + ')'
-		crossword.build_clues(across_clues, down_clues)
-		crossword.put()
-		self.redirect('/crossword?key=' + str(crossword.key()))
+				for clue in clues.findall('./' + self.namespace + 'clue'):
+					self.down_clue_dict[clue.get('number')] = clue.text + ' (' + clue.get('format') + ')'
