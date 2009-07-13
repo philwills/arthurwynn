59cc6543582501228b14ec994e302b18f71c7243
Phil Wills <otherphil@gmail.com>
2009-07-13 22:13:47 +0100

    Removed experimental user_crossword

--- arthurwynn/user_crossword.py
+++
@@ -1,9 +0,0 @@
-from crossword import *
-
-from google.appengine.ext import db
-
-class UserCrossword(db.Model):
-	user = db.UserProperty()
-	crossword = db.ReferenceProperty(Crossword)
-	answers = db.StringProperty()
-	created = db.DateTimeProperty(auto_now_add=True)
