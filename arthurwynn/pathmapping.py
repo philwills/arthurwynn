bae8b6c80d58290197ae86592b0985803e3f4a59
Phil Wills <philip@gnm40820.int.gnl>
2009-07-14 15:37:01 +0100

    Finished getting rid of UserCrossword stuff as never significantly implemented

--- arthurwynn/pathmapping.py
+++ arthurwynn/pathmapping.py
@@ -3,13 +3,11 @@ from arthurwynn.controller.upload import CrosswordUploadPage
 from arthurwynn.controller.create import CrosswordCreationPage, CrosswordGridPage, CrosswordCluePage
 from arthurwynn.controller.display import CrosswordPage, MicroappCrosswordPage
 from arthurwynn.controller.list import CrosswordListPage, CrosswordAtomPage, MicroappLatestPage
-from arthurwynn.controller.save import SaveCrosswordState
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from arthurwynn.crossword import *
-from arthurwynn.user_crossword import *
 
 application = webapp.WSGIApplication([
 									('/', CrosswordListPage),
@@ -20,7 +18,6 @@ application = webapp.WSGIApplication([
                                     ('/create/grid', CrosswordGridPage),
                                     ('/create/clues', CrosswordCluePage),
                                     ('/upload', CrosswordUploadPage),
-                                    ('/save', SaveCrosswordState),
                                     ('/atom.xml', CrosswordAtomPage),
 									], debug=True)
 		
