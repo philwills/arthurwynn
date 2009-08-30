3e963f814f0674fc84e70c02454f7bc9a8dfac41
Phil Wills <otherphil@gmail.com>
2009-08-30 14:25:00 +0100

    Extractor broken out of upload

---
+++ arthurwynn/controller/extractor.py
@@ -0,0 +1,67 @@
+from xml.etree.ElementTree import fromstring
+
+class CrosswordDotInfoXmlExtractor():
+	def parse(self, xml_string):
+		self.root = fromstring(xml_string)
+		self.namespace = '{http://crossword.info/xml/rectangular-puzzle}'
+		self.puzzle = self.root.find('./' + self.namespace + 'rectangular-puzzle/')
+		self.grid = self.puzzle.find('./' + self.namespace + 'crossword/' + self.namespace + 'grid')
+		self.across_clue_dict = {}
+		self.down_clue_dict = {}
+		self.extract_clues()
+
+	def title(self):
+		return self.puzzle.find('./' + self.namespace + 'metadata/' + self.namespace + 'title').text
+
+	def type(self):
+		titleText = self.title()
+		if titleText[0:4] == 'gdn.':
+			return titleText[4:]
+
+	def creator(self):
+		return self.puzzle.find('./' + self.namespace + 'metadata/' + self.namespace + 'creator').text
+		
+	def identifier(self):
+		return int(self.puzzle.find('./' + self.namespace + 'metadata/' + self.namespace + 'identifier').text)
+		
+	def width(self):
+		return int(self.grid.get('width'))
+		
+	def height(self):
+		return int(self.grid.get('height'))
+
+	def letters(self):
+		letters = {}
+		for cell in self.grid.findall('./' + self.namespace + 'cell'):
+			if cell.get('solution'):
+				letters[int(cell.get('x')), int(cell.get('y'))] = cell.get('solution')
+			else:
+				letters[int(cell.get('x')), int(cell.get('y'))] = ''
+		return letters
+
+	def across_clues(self):
+		return self.across_clue_dict
+
+	def down_clues(self):
+		return self.down_clue_dict
+
+	def extract_clues(self):
+		for clues in self.puzzle.findall('./' + self.namespace + 'crossword/' + self.namespace + 'clues'):
+			if (clues.find('./' + self.namespace + 'title/' + self.namespace +'b').text == 'Across'):
+				for clue in clues.findall('./' + self.namespace + 'clue'):
+					self.add_clue(self.across_clue_dict, clue)
+			else:
+				for clue in clues.findall('./' + self.namespace + 'clue'):
+					self.add_clue(self.down_clue_dict, clue)
+
+	def add_clue(self, clue_dict, clue):
+		clue_numbers = clue.get('number').split(',')
+		clue_dict[clue_numbers[0]] = self.get_clue_text(clue)
+		if (len(clue_numbers) > 1 and clue_numbers[1] not in clue_dict and not clue_numbers[1].endswith('down')):
+			clue_dict[clue_numbers[1]] = 'See ' + clue_numbers[0]
+
+	def get_clue_text(self, clue):
+		if (clue.get('format')):
+			return clue.text + ' (' + clue.get('format') + ')'
+		else:
+			return clue.text
---
+++ arthurwynn/controller/extractor_test.py
@@ -0,0 +1,81 @@
+import unittest
+
+import os
+
+from extractor import CrosswordDotInfoXmlExtractor
+
+class CrosswordDotInfoXmlExtractorTest(unittest.TestCase):
+	def setUp(self):
+		self.xml = open(os.path.join(os.path.dirname(__file__),'12110.xml.17'), 'r').read()
+		self.cryptic_xml = open(os.path.join(os.path.dirname(__file__),'24656.xml.16'), 'r').read()
+		self.extractor = CrosswordDotInfoXmlExtractor()
+
+	def test_should_extract_title_from_xml(self):
+		self.extractor.parse(self.xml)
+		self.assertEqual(self.extractor.title(), 'gdn.quick')
+
+	def test_should_get_type_from_title(self):
+		self.extractor.parse(self.xml)
+		self.assertEqual(self.extractor.type(), 'quick')
+
+	def test_should_extract_creator(self):
+		self.extractor.parse(self.xml)
+		self.assertEqual(self.extractor.creator(), 'Fiore')
+
+	def test_should_extract_identifier(self):
+		self.extractor.parse(self.xml)
+		self.assertEqual(self.extractor.identifier(), 12110)
+
+	def test_should_extract_width(self):
+		self.extractor.parse(self.xml)
+		self.assertEqual(self.extractor.width(), 13)
+
+	def test_should_extract_height(self):
+		self.extractor.parse(self.xml)
+		self.assertEqual(self.extractor.height(), 13)
+
+	def test_all_squares_should_be_in_letters_dictionary(self):
+		self.extractor.parse(self.xml)
+		letters = self.extractor.letters()
+		self.assertEqual(len(letters), 13 * 13)
+
+	def test_squares_with_a_letter_should_have_letter_in_dictionary(self):
+		self.extractor.parse(self.xml)
+		letters = self.extractor.letters()
+		self.assertEqual(letters[1,1], 'D')
+
+	def test_squares_with_no_letter_should_be_empty_string_in_dictionary(self):
+		self.extractor.parse(self.xml)
+		letters = self.extractor.letters()
+		self.assertEqual(letters[1,9], '')
+
+	def test_clues_should_be_in_directional_dictionaries_including_length(self):
+		self.extractor.parse(self.xml)
+		across = self.extractor.across_clues()
+		self.assertEqual(across['1'], 'Be bold enough (4)')
+		down = self.extractor.down_clues()
+		self.assertEqual(down['1'], 'Type of compass used for measuring distance (8)')
+
+	def test_should_be_able_to_extract_see_other_type_clue(self):
+		self.extractor.parse(self.cryptic_xml)
+		down = self.extractor.down_clues()
+		self.assertEqual(down['19'], 'See 4')
+
+	def test_should_extract_clue_where_answer_spans_multiple_numbers(self):
+		self.extractor.parse(self.cryptic_xml)
+		down = self.extractor.down_clues()
+		self.assertEqual(down['4'], 'Fearful hen I can, oddly, put in kind of curry (7,7)')
+		
+	def test_should_provide_entry_to_clue_array_even_if_only_in_joint_clue(self):
+		self.extractor.parse(self.cryptic_xml)
+		across = self.extractor.across_clues()
+		self.assertTrue('17' in across)
+		self.assertEqual(across['17'], 'See 15')
+
+	def test_should_not_put_24_in_across_dictionary(self):
+		self.extractor.parse(self.cryptic_xml)
+		across = self.extractor.across_clues()
+		self.assertFalse('24down' in across)
+
+if __name__ == '__main__':
+	unittest.main()
