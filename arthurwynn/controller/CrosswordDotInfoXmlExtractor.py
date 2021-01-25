f2cd35ed0a3086a816bb189426465a3a6bf281ab
Phil Wills <otherphil@gmail.com>
2021-01-25 22:23:37 +0000

    Move to a single parse function in the interface

--- arthurwynn/controller/CrosswordDotInfoXmlExtractor.py
+++ arthurwynn/controller/CrosswordDotInfoXmlExtractor.py
@@ -5,15 +5,18 @@ from typing import Dict, Optional, Tuple
 from dataclasses import dataclass
 from enum import Enum, unique
 
+
 @unique
 class CrosswordType(Enum):
     QUICK = "quick"
     CRYPTIC = "cryptic"
 
+
 @dataclass(frozen=True)
 class Coordinate:
     x: int
     y: int
+
 
 @dataclass(frozen=True)
 class Crossword:
@@ -27,89 +30,96 @@ class Crossword:
     across_clues: Dict[str, str]
     down_clues: Dict[str, str]
 
-class CrosswordDotInfoXmlExtractor:
-    ns = "{http://crossword.info/xml/rectangular-puzzle}"
 
-    def parse(self, xml_string) -> Crossword:
-        self.root = fromstring(xml_string)
-        self.puzzle = self.root.find(f"./{self.ns}rectangular-puzzle")
-        self.grid = self.puzzle.find(f"./{self.ns}crossword/{self.ns}grid")
-        self.across_clue_dict = {}
-        self.down_clue_dict = {}
-        self._extract_clues()
-        return Crossword(
-            title=self.title(),
-            type = self.type(),
-            identifier = self.identifier(),
-            creator = self.creator(),
-            height = self.height,
-            width = self.width,
-            letters = self.letters(),
-            across_clues = self.across_clues(),
-            down_clues = self.down_clues(),
-        )
+_ns = "{http://crossword.info/xml/rectangular-puzzle}"
 
-    def title(self) -> str:
-        return f"{self.type()._value_.capitalize()} Crossword No. {self.identifier()}"
 
-    def type(self) -> Optional[CrosswordType]:
-        titleText = self.puzzle.find(f"./{self.ns}metadata/{self.ns}title").text
-        if titleText[0:4] == "gdn.":
-            return CrosswordType(titleText[4:])
+def parse(xml_string) -> Crossword:
+    root = fromstring(xml_string)
+    puzzle = root.find(f"./{_ns}rectangular-puzzle")
+    grid = puzzle.find(f"./{_ns}crossword/{_ns}grid")
+    (across_clues, down_clues) = _extract_clues(puzzle)
+
+    return Crossword(
+        title=_title(puzzle),
+        type=_type(puzzle),
+        identifier=_identifier(puzzle),
+        creator=_creator(puzzle),
+        height=_height(grid),
+        width=_width(grid),
+        letters=_letters(grid),
+        across_clues=across_clues,
+        down_clues=down_clues,
+    )
+
+
+def _title(puzzle) -> str:
+    return f"{_type(puzzle)._value_.capitalize()} Crossword No. {_identifier(puzzle)}"
+
+
+def _type(puzzle) -> Optional[CrosswordType]:
+    titleText = puzzle.find(f"./{_ns}metadata/{_ns}title").text
+    if titleText[0:4] == "gdn.":
+        return CrosswordType(titleText[4:])
+    else:
+        return None
+
+
+def _creator(puzzle) -> str:
+    return puzzle.find(f"./{_ns}metadata/{_ns}creator").text
+
+
+def _identifier(puzzle) -> int:
+    return int(puzzle.find(f"./{_ns}metadata/{_ns}identifier").text)
+
+
+def _width(grid) -> int:
+    return int(grid.get("width"))
+
+
+def _height(grid) -> int:
+    return int(grid.get("height"))
+
+
+def _letters(grid) -> Dict[Coordinate, str]:
+    letters = {}
+    for cell in grid.findall(f"./{_ns}cell"):
+        if cell.get("solution"):
+            letters[
+                Coordinate(int(cell.get("x")) - 1, int(cell.get("y")) - 1)
+            ] = cell.get("solution")
         else:
-            return None
+            letters[Coordinate(int(cell.get("x")) - 1, int(cell.get("y")) - 1)] = ""
+    return letters
 
-    def creator(self) -> str:
-        return self.puzzle.find(f"./{self.ns}metadata/{self.ns}creator").text
 
-    def identifier(self) -> int:
-        return int(self.puzzle.find(f"./{self.ns}metadata/{self.ns}identifier").text)
-
-    def width(self) -> int:
-        return int(self.grid.get("width"))
-
-    def height(self) -> int:
-        return int(self.grid.get("height"))
-
-    def letters(self) -> Dict[Coordinate, str]:
-        letters = {}
-        for cell in self.grid.findall(f"./{self.ns}cell"):
-            if cell.get("solution"):
-                letters[
-                    Coordinate(int(cell.get("x")) - 1, int(cell.get("y")) - 1)
-                ] = cell.get("solution")
-            else:
-                letters[Coordinate(int(cell.get("x")) - 1, int(cell.get("y")) - 1)] = ""
-        return letters
-
-    def across_clues(self) -> Dict[int, str]:
-        return self.across_clue_dict
-
-    def down_clues(self) -> Dict[int, str]:
-        return self.down_clue_dict
-
-    def _extract_clues(self):
-        for clues in self.puzzle.findall(f"./{self.ns}crossword/{self.ns}clues"):
-            if clues.find(f"./{self.ns}title/{self.ns}b").text == "Across":
-                for clue in clues.findall(f"./{self.ns}clue"):
-                    self._add_clue(self.across_clue_dict, clue)
-            else:
-                for clue in clues.findall(f"./{self.ns}clue"):
-                    self._add_clue(self.down_clue_dict, clue)
-
-    def _add_clue(self, clue_dict, clue):
-        clue_numbers = clue.get("number").split(",")
-        clue_dict[clue_numbers[0]] = self._get_clue_text(clue)
-        if (
-            len(clue_numbers) > 1
-            and clue_numbers[1] not in clue_dict
-            and not clue_numbers[1].endswith("down")
-        ):
-            clue_dict[clue_numbers[1]] = f"See {clue_numbers[0]}"
-
-    def _get_clue_text(self, clue):
-        format = clue.get("format")
-        if format:
-            return f"{clue.text} ({format})"
+def _extract_clues(puzzle) -> Tuple[Dict[int, str], Dict[int, str]]:
+    across_clue_dict = {}
+    down_clue_dict = {}
+    for clues in puzzle.findall(f"./{_ns}crossword/{_ns}clues"):
+        if clues.find(f"./{_ns}title/{_ns}b").text == "Across":
+            for clue in clues.findall(f"./{_ns}clue"):
+                _add_clue(across_clue_dict, clue)
         else:
-            return clue.text
+            for clue in clues.findall(f"./{_ns}clue"):
+                _add_clue(down_clue_dict, clue)
+    return (across_clue_dict, down_clue_dict)
+
+
+def _add_clue(clue_dict, clue):
+    clue_numbers = clue.get("number").split(",")
+    clue_dict[clue_numbers[0]] = _get_clue_text(clue)
+    if (
+        len(clue_numbers) > 1
+        and clue_numbers[1] not in clue_dict
+        and not clue_numbers[1].endswith("down")
+    ):
+        clue_dict[clue_numbers[1]] = f"See {clue_numbers[0]}"
+
+
+def _get_clue_text(clue):
+    format = clue.get("format")
+    if format:
+        return f"{clue.text} ({format})"
+    else:
+        return clue.text
--- arthurwynn/controller/CrosswordDotInfoXmlExtractorTest.py
+++ arthurwynn/controller/CrosswordDotInfoXmlExtractorTest.py
@@ -2,7 +2,7 @@ import unittest
 
 import os
 
-from CrosswordDotInfoXmlExtractor import CrosswordDotInfoXmlExtractor, Coordinate
+from CrosswordDotInfoXmlExtractor import parse, Coordinate
 
 
 class CrosswordDotInfoXmlExtractorTest(unittest.TestCase):
@@ -13,75 +13,74 @@ class CrosswordDotInfoXmlExtractorTest(unittest.TestCase):
         self.cryptic_xml = open(
             os.path.join(os.path.dirname(__file__), "24656.xml.16"), "r"
         ).read()
-        self.extractor = CrosswordDotInfoXmlExtractor()
 
     def test_should_extract_title_based_on_type_and_identifier(self):
-        crossword = self.extractor.parse(self.xml)
+        crossword = parse(self.xml)
         self.assertEqual(crossword.title, "Quick Crossword No. 12110")
 
     def test_should_get_type_from_title(self):
-        crossword = self.extractor.parse(self.xml)
+        crossword = parse(self.xml)
         self.assertEqual(crossword.type._value_, "quick")
 
     def test_should_extract_creator(self):
-        crossword = self.extractor.parse(self.xml)
+        crossword = parse(self.xml)
         self.assertEqual(crossword.creator, "Fiore")
 
     def test_should_extract_identifier(self):
-        crossword = self.extractor.parse(self.xml)
+        crossword = parse(self.xml)
         self.assertEqual(crossword.identifier, 12110)
 
     def test_should_extract_width(self):
-        crossword = self.extractor.parse(self.xml)
+        crossword = parse(self.xml)
         self.assertEqual(crossword.width, 13)
 
     def test_should_extract_height(self):
-        crossword = self.extractor.parse(self.xml)
+        crossword = parse(self.xml)
         self.assertEqual(crossword.height, 13)
 
     def test_all_squares_should_be_in_letters_dictionary(self):
-        crossword = self.extractor.parse(self.xml)
+        crossword = parse(self.xml)
         letters = crossword.letters
         self.assertEqual(len(letters), 13 * 13)
 
     def test_squares_with_a_letter_should_have_letter_in_dictionary(self):
-        crossword = self.extractor.parse(self.xml)
-        letters = crossword.letters()
+        crossword = parse(self.xml)
+        letters = crossword.letters
         self.assertEqual(letters[Coordinate(0, 0)], "D")
 
     def test_squares_with_no_letter_should_be_empty_string_in_dictionary(self):
-        crossword = self.extractor.parse(self.xml)
-        letters = crossword.letters()
+        crossword = parse(self.xml)
+        letters = crossword.letters
         self.assertEqual(letters[Coordinate(0, 8)], "")
 
     def test_clues_should_be_in_directional_dictionaries_including_length(self):
-        crossword = self.extractor.parse(self.xml)
-        across = crossword.across_clues()
+        crossword = parse(self.xml)
+        across = crossword.across_clues
         self.assertEqual(across["1"], "Be bold enough (4)")
-        down = crossword.down_clues()
+        down = crossword.down_clues
         self.assertEqual(down["1"], "Type of compass used for measuring distance (8)")
 
     def test_should_be_able_to_extract_see_other_type_clue(self):
-        crossword = self.extractor.parse(self.cryptic_xml)
-        down = crossword.down_clues()
+        crossword = parse(self.cryptic_xml)
+        down = crossword.down_clues
         self.assertEqual(down["19"], "See 4")
 
     def test_should_extract_clue_where_answer_spans_multiple_numbers(self):
-        crossword = self.extractor.parse(self.cryptic_xml)
-        down = crossword.down_clues()
+        crossword = parse(self.cryptic_xml)
+        down = crossword.down_clues
         self.assertEqual(
             down["4"], "Fearful hen I can, oddly, put in kind of curry (7,7)"
         )
 
     def test_should_provide_entry_to_clue_array_even_if_only_in_joint_clue(self):
-        crossword = self.extractor.parse(self.cryptic_xml)
-        across = crossword.across_clues()
+        crossword = parse(self.cryptic_xml)
+        across = crossword.across_clues
         self.assertTrue("17" in across)
         self.assertEqual(across["17"], "See 15")
 
     def test_should_not_put_24_in_across_dictionary(self):
-        crossword = self.extractor.parse(self.cryptic_xml)
-        across = crossword.across_clues()
+        crossword = parse(self.cryptic_xml)
+        across = crossword.across_clues
         self.assertFalse("24down" in across)
 
 if __name__ == "__main__":
